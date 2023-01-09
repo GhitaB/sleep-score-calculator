17870d83fd2e919f7bd22e21a61ddce1c31c8307
GhitaB <ghitabizau27@gmail.com>
2023-01-09 22:10:44 +0200

    Add react select.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Select from 'react-select'
+
+const hours = [
+  { value: 8, label: '8 hours' },
+  { value: 7.5, label: '7.5 hours' },
+  { value: 7, label: '7 hours' },
+  { value: 6.5, label: '6.5 hours' },
+  { value: 6, label: '6 hours' },
+  { value: 5.5, label: '5.5 hours' },
+  { value: 5, label: '5 hours' },
+  { value: 4.5, label: '4.5 hours' },
+  { value: 4, label: '4 hours' },
+  { value: 3.5, label: '3.5 hours' },
+  { value: 3, label: '3 hours' },
+]
+
+const SelectHours = () => (
+  <Select options={hours} />
+)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>10</h1>
+      <SelectHours />
     </div>
   );
 }
